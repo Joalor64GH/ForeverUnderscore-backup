16e1b058cf8ea9a3638c3baaf1a18fd358969f8f
BeastlyGhost <45212377+BeastlyGhost@users.noreply.github.com>
2022-08-08 16:57:46 -0400

    Update Overlay.hx

--- source/base/ui/Overlay.hx
+++ source/base/ui/Overlay.hx
@@ -68,8 +68,7 @@ class Overlay extends TextField
 			text = '' // set up the text itself
 				+ (displayFps ? times.length + " FPS\n" : '') // Framerate
 			#if !neko + (displayExtra ? Main.mainClassState + "\n" : '') #end // Current Game State
-			+ (displayMemory ? '${getInterval(mem)} /' // Current Memory Usage
-				+ ' ${getInterval(memPeak)}\n' : ''); // Total Memory Usage
+			+ (displayMemory ? '${getInterval(mem)} / ${getInterval(memPeak)}\n' : ''); // Current and Total Memory Usage
 		}
 	}
 
