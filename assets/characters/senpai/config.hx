d92413a92a90f680b659c35c532a4897df02f571
Gabi <45212377+BeastlyGhost@users.noreply.github.com>
2022-08-28 19:19:23 -0400

    Update senpai/config.hx

--- assets/characters/senpai/config.hx
+++ assets/characters/senpai/config.hx
@@ -1,33 +1,9 @@
 function loadAnimations()
 {
-    if (song == 'roses')
-    {
-        addByPrefix('idle', 'Angry Idle', 24, false);
-        addByPrefix('singUP', 'Angry Up', 24, false);
-        addByPrefix('singLEFT', 'Angry Left', 24, false);
-        addByPrefix('singRIGHT', 'Angry Right', 24, false);
-        addByPrefix('singDOWN', 'Angry Down', 24, false);
-
-        addOffset('idle', 110, 280);
-        addOffset('singUP', 115, 282);
-        addOffset('singRIGHT', 110, 280);
-        addOffset('singLEFT', 150, 280);
-        addOffset('singDOWN', 124, 280);
-    }
+    if (StringTools.startsWith(song, 'roses'))
+        getMad();
     else
-    {
-        addByPrefix('idle', 'Idle', 24, false);
-        addByPrefix('singUP', 'Up', 24, false);
-        addByPrefix('singLEFT', 'Left', 24, false);
-        addByPrefix('singRIGHT', 'Right', 24, false);
-        addByPrefix('singDOWN', 'Down', 24, false);
-
-        addOffset('idle', 110, 280);
-        addOffset('singUP', 115, 282);
-        addOffset('singRIGHT', 110, 280);
-        addOffset('singLEFT', 150, 280);
-        addOffset('singDOWN', 124, 280);
-    }
+        setupAnims();
 
     setGraphicSize(get('width') * 6);
     set('antialiasing', false);
@@ -38,4 +14,34 @@ function loadAnimations()
     setCamOffsets(-230, -590);
     setBarColor([255,170,111]);
     quickDancer(false);
+}
+
+function setupAnims()
+{
+    addByPrefix('idle', 'Idle', 24, false);
+    addByPrefix('singUP', 'Up', 24, false);
+    addByPrefix('singLEFT', 'Left', 24, false);
+    addByPrefix('singRIGHT', 'Right', 24, false);
+    addByPrefix('singDOWN', 'Down', 24, false);
+
+    addOffset('idle', 110, 280);
+    addOffset('singUP', 115, 282);
+    addOffset('singRIGHT', 110, 280);
+    addOffset('singLEFT', 150, 280);
+    addOffset('singDOWN', 124, 280);
+}
+
+function getMad() // cope;
+{
+    addByPrefix('idle', 'Angry Idle', 24, false);
+    addByPrefix('singUP', 'Angry Up', 24, false);
+    addByPrefix('singLEFT', 'Angry Left', 24, false);
+    addByPrefix('singRIGHT', 'Angry Right', 24, false);
+    addByPrefix('singDOWN', 'Angry Down', 24, false);
+
+    addOffset('idle', 110, 280);
+    addOffset('singUP', 115, 282);
+    addOffset('singRIGHT', 110, 280);
+    addOffset('singLEFT', 150, 280);
+    addOffset('singDOWN', 124, 280);
 }
