b132c4133e8db092499a0a9cac046899624174e9
Gabi <45212377+BeastlyGhost@users.noreply.github.com>
2022-08-28 19:30:31 -0400

    update setup files

--- art/setupFiles/setup.bat
+++ art/setupFiles/setup.bat
@@ -25,7 +25,7 @@ pause >nul
 title FNF Setup - Installing libraries
 haxelib git polymod https://github.com/larsiusprime/polymod.git
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib git SScript https://github.com/TheWorldMachine/SScript
+haxelib git SScript https://github.com/BeastlyGhost/SScript-OpenFL
 cls
 goto CommunitySetup
 
