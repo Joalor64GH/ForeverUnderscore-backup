d009a2860538b849fe23e4e1da855de7b10827bf
Gabi <45212377+BeastlyGhost@users.noreply.github.com>
2022-08-30 13:08:07 -0400

    bf-pixel-dead offsets

--- assets/characters/bf-pixel-dead/config.hx
+++ assets/characters/bf-pixel-dead/config.hx
@@ -4,12 +4,25 @@ function loadAnimations()
     addByPrefix('deathLoop', "Retry Loop", 24, true);
     addByPrefix('deathConfirm', "RETRY CONFIRM", 24, false);
 
-    addOffset('firstDeath', 0, 0);
-    addOffset('deathLoop', -30, -10);
-    addOffset('deathConfirm', 3, 0);
+    addOffset('firstDeath', 0, -20);
+    addOffset('deathLoop', -30, -20);
+    addOffset('deathConfirm', 0, -20);
 
     setGraphicSize(get('width') * 6);
     set('antialiasing', false);
+
+    if (curStage == 'stage')
+    {
+        setOffsets(0, 910);
+        setCamOffsets(-30, -150);
+    }
+    if (curStage == 'school' || curStage == 'schoolEvil')
+        setOffsets(15, 400);
+    if (curStage == 'school')
+        setCamOffsets(50, -80);
+    if (curStage == 'schoolEvil')
+        setCamOffsets(200, -80);
+
     if (isPlayer)
         set('flipX', true);
     else
