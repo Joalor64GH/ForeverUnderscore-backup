1daa2a3f9ed8f1508d84ac0385247b6955f0bddb
Gabi <45212377+BeastlyGhost@users.noreply.github.com>
2022-09-11 12:43:48 -0400

    fix reduced movements on milf events

--- assets/songs/milf/zooming.hx
+++ assets/songs/milf/zooming.hx
@@ -1,7 +1,7 @@
 function beatHit(curBeat)
 {
     if (curBeat >= 168 && curBeat < 200
-		&& !getSetting('Reduced Movements')
+		&& !Init.trueSettings.get('Reduced Movements')
 		&& FlxG.camera.zoom < 1.35)
 	{
 		FlxG.camera.zoom += 0.015;
