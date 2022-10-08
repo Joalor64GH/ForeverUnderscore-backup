f5ac660346c31a1fb54948f9ea616653f908e814
Gabi <45212377+BeastlyGhost@users.noreply.github.com>
2022-10-08 14:12:17 -0400

    oops

--- source/funkin/Strumline.hx
+++ source/funkin/Strumline.hx
@@ -30,8 +30,7 @@ class Strumline extends FlxSpriteGroup
 	public var autoplay:Bool = true;
 	public var displayJudgements:Bool = false;
 
-	public function new(xPos:Float = 0, yPos:Float = 0, ?character:Character, ?autoplay:Bool = true, ?displayJudgements:Bool = false, ?keyAmount:Int = 4,
-			?parent:Strumline)
+	public function new(xPos:Float = 0, yPos:Float = 0, ?character:Character, ?autoplay:Bool = true, ?displayJudgements:Bool = false, ?keyAmount:Int = 4)
 	{
 		super();
 
