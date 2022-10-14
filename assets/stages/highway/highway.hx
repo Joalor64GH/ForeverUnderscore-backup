ef9aa9d757802170b36dc9110726e65a0d31835e
Gabriela <45212377+BeastlyGhost@users.noreply.github.com>
2022-10-14 17:48:05 -0400

    Update highway.hx

--- assets/stages/highway/highway.hx
+++ assets/stages/highway/highway.hx
@@ -54,7 +54,7 @@ function resetFastCar():Void
 var carTimer:FlxTimer;
 function fastCarDrive()
 {
-	trace('Car drive');
+	//trace('Car drive');
 	FlxG.sound.play(Paths.soundRandom('carPass', 0, 1), 0.7);
 
 	fastCar.velocity.x = (FlxG.random.int(170, 220) / FlxG.elapsed) * 3;
